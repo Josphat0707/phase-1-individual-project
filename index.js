b3dcf5d49a2aeefc7a253a819d1b0c7a5499c453
josphat <mjirura0707@gmail.com>
2023-07-05 21:56:35 +0300

    Started working on the js file, added some event listeners and managed to fetch data from the public api

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-document.addEventListener(DOMContentLoaded())
+const url = "www.thecocktaildb.com/api/json/v1/1/search.php?s"
+
+// Fetch data from the public API
+document.addEventListener(`DOMContentLoaded`, function(){
+   function searchCocktail(searchValue){ 
+    fetch(url)
+    .then(res => res.json)
+    .then(data => {
+        displayCocktails(data.drinks);
+    });
+}
+
+    document.querySelector(`form`).addEventListener(`submit`, function(event){
+        event.preventDefault();
+        const searchValue = document.querySelector(`search`).value;
+        searchCocktail(searchValue);
+    });
+
+    function displayCocktails(cocktails){
+        const cocktailList = document.querySelector(`#cocktail-list`);
+        cocktailList.innerHTML = ``;
+
+        if(cocktails){
+            cocktails.forEach(cocktail => {
+                const cocktailItem = document.createElement(`li`);
+            })
+        }
+    }
+});

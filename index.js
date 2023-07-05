24bd2e5efe15ef23b79eabb5be45116d1c5c3565
josphat <mjirura0707@gmail.com>
2023-07-05 22:06:55 +0300

    Adding comments to the code as i continue writing it

--- index.js
+++ index.js
@@ -15,15 +15,26 @@ document.addEventListener(`DOMContentLoaded`, function(){
         const searchValue = document.querySelector(`search`).value;
         searchCocktail(searchValue);
     });
-
+//Created a function that handles the response acquired from the Public API
     function displayCocktails(cocktails){
         const cocktailList = document.querySelector(`#cocktail-list`);
         cocktailList.innerHTML = ``;
 
+//added a list that will store and show the results after searching for a drink
         if(cocktails){
             cocktails.forEach(cocktail => {
                 const cocktailItem = document.createElement(`li`);
-            })
-        }
+                cocktailItem.textContent = cocktail.strDrink;
+                cocktailItem.addEventListener(`click`, function(){
+                    getCocktailDetails(cocktail.idDrink);
+                });
+                cocktailList.appendChild(cocktailItem);
+            });
+        } else {
+            // incase the drink searched is not available i have created a list element that will dislpay a message 
+            const message = document.createElement(`li`);
+            message.textContent = `Cocktail cannot be found`;
+            cocktailList.appendChild(message);
+        };
     }
 });
